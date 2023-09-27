c1390211ea2f8eaf27111076a715c451e02da6a1
Officeyutong <yt.xyxx@gmail.com>
2023-09-28 07:26:31 +0800

    Tools: add a new CLI in rust and related CI tests (#12)
    * Update cli with static linking
    
    * Initial testing CLI CI
    
    * Fix CI
    
    * Fix CI
    
    * Fix CI

--- tools/cli-rs/src/main.rs
+++ tools/cli-rs/src/main.rs
@@ -28,6 +28,7 @@ enum SubCommand {
     },
     #[clap(about = "Inject bpftime-agent to a certain pid")]
     Attach { pid: i32 },
+    #[cfg(feature = "support-load-bpf")]
     #[clap(about = "Load and attach an eBPF object into kernel")]
     LoadBpf {
         #[arg(help = "Path to the ELF file")]
@@ -88,6 +89,7 @@ fn inject_by_frida(pid: i32, agent: impl AsRef<Path>) -> anyhow::Result<()> {
     Ok(())
 }
 
+#[cfg(feature = "support-load-bpf")]
 fn load_ebpf_object_into_kernel(path: impl AsRef<Path>) -> anyhow::Result<()> {
     let mut obj = libbpf_rs::ObjectBuilder::default()
         .open_file(path.as_ref())
@@ -139,9 +141,10 @@ fn main() -> anyhow::Result<()> {
             if !so_path.exists() {
                 bail!("Library not found: {:?}", so_path);
             }
-            println!("Inject: {:?}",so_path);
+            println!("Inject: {:?}", so_path);
             inject_by_frida(pid, so_path)
         }
+        #[cfg(feature = "support-load-bpf")]
         SubCommand::LoadBpf { path } => load_ebpf_object_into_kernel(PathBuf::from(path))
             .with_context(|| anyhow!("Failed to load ebpf object into kernel")),
     }
