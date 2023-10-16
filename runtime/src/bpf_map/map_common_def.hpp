12d8956faa082d85c7fd19f7bd1abfa2701fe548
Officeyutong <yt.xyxx@gmail.com>
2023-10-17 00:11:02 +0800

    runtime: add new test-framework for bpftime-runtime (#38)
    * Update
    
    Initial
    
    runtime: fix a double-free bug (#37)
    
    * Fix a CI issue
    
    * Update
    
    Update a per cpu array test
    
    * Update a per cpu hash test
    
    * Rename some files
    
    * Update something
    
    * Fix CI

--- runtime/src/bpf_map/map_common_def.hpp
+++ runtime/src/bpf_map/map_common_def.hpp
@@ -14,7 +14,8 @@ using bytes_vec_allocator = boost::interprocess::allocator<
 	uint8_t, boost::interprocess::managed_shared_memory::segment_manager>;
 using bytes_vec = boost::interprocess::vector<uint8_t, bytes_vec_allocator>;
 
-template <class T> T ensure_on_current_cpu(std::function<T(int cpu)> func)
+template <class T>
+static inline T ensure_on_current_cpu(std::function<T(int cpu)> func)
 {
 	cpu_set_t orig, set;
 	CPU_ZERO(&orig);
@@ -28,6 +29,32 @@ template <class T> T ensure_on_current_cpu(std::function<T(int cpu)> func)
 	return ret;
 }
 
+template <class T>
+static inline T ensure_on_certain_cpu(int cpu, std::function<T()> func)
+{
+	cpu_set_t orig, set;
+	CPU_ZERO(&orig);
+	CPU_ZERO(&set);
+	sched_getaffinity(0, sizeof(orig), &orig);
+	CPU_SET(cpu, &set);
+	sched_setaffinity(0, sizeof(set), &set);
+	T ret = func();
+	sched_setaffinity(0, sizeof(orig), &orig);
+	return ret;
+}
+
+template <>
+inline void ensure_on_certain_cpu(int cpu, std::function<void()> func)
+{
+	cpu_set_t orig, set;
+	CPU_ZERO(&orig);
+	CPU_ZERO(&set);
+	sched_getaffinity(0, sizeof(orig), &orig);
+	CPU_SET(cpu, &set);
+	sched_setaffinity(0, sizeof(set), &set);
+	func();
+	sched_setaffinity(0, sizeof(orig), &orig);
+}
 struct bytes_vec_hasher {
 	size_t operator()(bytes_vec const &vec) const
 	{
