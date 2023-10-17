d82736f333652e95afad15b120da615659f4c2e0
Officeyutong <yt.xyxx@gmail.com>
2023-10-17 19:26:39 +0800

    Use cmake to build bpftime-daemon (#43)
    * stage changes
    
    * Update
    
    * Fix Ci
    
    * Fix Ci

--- cmake/libbpf.cmake
+++ cmake/libbpf.cmake
@@ -1,4 +1,3 @@
-
 #
 # Setup libbpf
 #
@@ -61,3 +60,49 @@ foreach(file ${HEADER_FILES})
 endforeach()
 
 add_dependencies(copy_headers libbpf)
+
+# # Setup bpftool
+set(BPFTOOL_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party/bpftool)
+set(BPFTOOL_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/bpftool)
+ExternalProject_Add(bpftool
+  PREFIX bpftool
+  SOURCE_DIR ${BPFTOOL_DIR}/src
+  CONFIGURE_COMMAND "mkdir" "-p" "${BPFTOOL_INSTALL_DIR}"
+  BUILD_COMMAND "make" "EXTRA_CFLAGS=-g -O2 -Wall -Werror " "-j"
+  INSTALL_COMMAND "cp" "${BPFTOOL_DIR}/src/bpftool" "${BPFTOOL_INSTALL_DIR}/bpftool"
+  BUILD_IN_SOURCE TRUE
+  BUILD_BYPRODUCTS ${BPFTOOL_DIR}/src/bpftool
+  INSTALL_BYPRODUCTS ${BPFTOOL_INSTALL_DIR}/bpftool
+)
+
+function(add_bpf_skel_generating_target target_name bpf_program output_skel)
+  add_custom_target(${target_name} ALL
+    COMMAND "${BPFTOOL_INSTALL_DIR}/bpftool" "gen" "skeleton" "${bpf_program}" > "${output_skel}"
+    BYPRODUCTS ${output_skel}
+    SOURCES ${bpf_program}
+    DEPENDS bpftool
+
+    )
+endfunction()
+
+# Define a helper function
+function(add_ebpf_program_target target_name source_file output_file)
+  # opensnoop.bpf.o
+  execute_process(COMMAND bash -c "uname -m | sed 's/x86_64/x86/' \
+| sed 's/arm.*/arm/' \
+| sed 's/aarch64/arm64/' \
+| sed 's/ppc64le/powerpc/' \
+| sed 's/mips.*/mips/' \
+| sed 's/riscv64/riscv/' \
+| sed 's/loongarch64/loongarch/'"
+    OUTPUT_VARIABLE UNAME_ARCH
+    COMMAND_ERROR_IS_FATAL ANY
+  )
+  string(STRIP ${UNAME_ARCH} UNAME_ARCH_STRIPPED)
+  add_custom_target(${target_name} ALL
+    COMMAND clang -O2 -target bpf -c -g -D__TARGET_ARCH_${UNAME_ARCH_STRIPPED} -I${CMAKE_SOURCE_DIR}/third_party/vmlinux/${UNAME_ARCH_STRIPPED} -I${LIBBPF_INCLUDE_DIRS}/uapi -I${LIBBPF_INCLUDE_DIRS} ${source_file} -o ${output_file}
+    BYPRODUCTS ${output_file}
+    SOURCES ${source_file}
+  )
+  add_dependencies(${target_name} copy_headers)
+endfunction()
