c90a70f079b5e93340561fd595de8f0e756022dd
officeyutong <yt.xyxx@gmail.com>
2023-11-05 23:01:21 +0800

    update DEFAULT_MAX_FD

--- runtime/src/handler/handler_manager.hpp
+++ runtime/src/handler/handler_manager.hpp
@@ -37,7 +37,7 @@ using boost_shm_string =
 	boost::interprocess::basic_string<char, std::char_traits<char>,
 					  char_allocator>;
 
-const size_t DEFAULT_MAX_FD = 1024 * 8;
+const size_t DEFAULT_MAX_FD = 1024 * 6;
 
 struct unused_handler {};
 
