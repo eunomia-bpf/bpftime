f3599fce80dbf046485b4394ebe8b322533f5dd2
yunwei37 <1067852565@qq.com>
2023-11-22 21:54:44 +0000

    add bench for uprobe override

---
+++ benchmark/uprobe/uprobe-override.bpf.c
@@ -0,0 +1,12 @@
+#define BPF_NO_GLOBAL_DATA
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+SEC("uprobe")
+int do_uprobe_override_patch(struct pt_regs *ctx)
+{
+	return 0;
+}
+
+char LICENSE[] SEC("license") = "GPL";
---
+++ benchmark/uprobe/uprobe-override.c
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+/* Copyright (c) 2020 Facebook */
+#include <signal.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdint.h>
+#include <sys/resource.h>
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "uprobe-override.skel.h"
+#include <inttypes.h>
+#include "attach_override.h"
+
+#define warn(...) fprintf(stderr, __VA_ARGS__)
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format,
+			   va_list args)
+{
+	return vfprintf(stderr, format, args);
+}
+
+static volatile bool exiting = false;
+
+static void sig_handler(int sig)
+{
+	exiting = true;
+}
+
+int main(int argc, char **argv)
+{
+	struct uprobe_override_bpf *skel;
+	int err;
+
+	/* Set up libbpf errors and debug info callback */
+	libbpf_set_print(libbpf_print_fn);
+
+	/* Cleaner handling of Ctrl-C */
+	signal(SIGINT, sig_handler);
+	signal(SIGTERM, sig_handler);
+
+	/* Load and verify BPF application */
+	skel = uprobe_override_bpf__open();
+	if (!skel) {
+		fprintf(stderr, "Failed to open and load BPF skeleton\n");
+		return 1;
+	}
+
+	/* Load & verify BPF programs */
+	err = uprobe_override_bpf__load(skel);
+	if (err) {
+		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
+		goto cleanup;
+	}
+	err = bpf_prog_attach_uprobe_with_override(
+		bpf_program__fd(skel->progs.do_uprobe_override_patch), "benchmark/test",
+		"__benchmark_test_function1");
+	if (err) {
+		fprintf(stderr, "Failed to attach BPF program\n");
+		goto cleanup;
+	}
+	while (!exiting) {
+		sleep(1);
+	}
+cleanup:
+	/* Clean up */
+	uprobe_override_bpf__destroy(skel);
+	return err < 0 ? -err : 0;
+}
