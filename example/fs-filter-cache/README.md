5f6affc17fbac3179d0e616aea75425b58c792df
yunwei37 <1067852565@qq.com>
2023-11-23 23:40:17 +0000

    improve bench md

--- example/fs-filter-cache/README.md
+++ example/fs-filter-cache/README.md
@@ -11,3 +11,39 @@ run agent
 ```console
 # AGENT_SO=build/runtime/agent/libbpftime-agent.so LD_PRELOAD=build/runtime/agent-transformer/libbpftime-agent-transformer.so find ./example
 ```
+
+run server:
+
+```console
+sudo LD_PRELOAD=build/runtime/syscall-server/libbpftime-syscall-server.so example/fs-filter-cache/fs-cache
+```
+
+## cache for getdents64
+
+passthrough fuse base line of getdents64 syscall
+
+```console
+root@yunwei37server:/home/yunwei/bpftime# /home/yunwei/bpftime/example/fs-filter-cache/bench getdents64 /home/yunwei/bpftime-evaluation/fuse/data/virt
+inode=4294967295 offset=32 reclen=24 type=0 name=.
+inode=4294967295 offset=64 reclen=24 type=0 name=..
+inode=4294967295 offset=96 reclen=24 type=0 name=kvm
+inode=4294967295 offset=128 reclen=24 type=0 name=lib
+inode=4294967295 offset=160 reclen=32 type=0 name=Makefile
+Average time usage 36526.149010 ns, iter 100000 times
+```
+
+use cache for getdents64 syscall
+
+```console
+# AGENT_SO=build/runtime/agent/libbpftime-agent.so LD_PRELOAD=build/runtime/agent-transformer/libbpftime-agent-transformer.so  /home/yunwei/bpftime/example/fs-filter-cache/bench getdents64  /home/yunwei/bpftime-evaluation/fuse/data/virt
+....
+inode=4294967295 offset=32 reclen=24 type=0 name=.
+inode=4294967295 offset=64 reclen=24 type=0 name=..
+inode=4294967295 offset=96 reclen=24 type=0 name=kvm
+inode=4294967295 offset=128 reclen=24 type=0 name=lib
+inode=4294967295 offset=160 reclen=32 type=0 name=Makefile
+Average time usage 1761.278320 ns, iter 100000 times
+
+[2023-11-23 23:38:52][info][414216] Compiling using LLJIT
+INFO [414216]: Global shm destructed
+```
--- example/fs-filter-cache/bench.cpp
+++ example/fs-filter-cache/bench.cpp
@@ -4,7 +4,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/syscall.h>
+#include <string.h>
 #include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 void start_timer(struct timespec *start_time)
 {
@@ -25,7 +30,6 @@ struct linux_dirent64 {
 };
 
 #define BUF_SIZE 2048
-
 
 static double get_elapsed_time(struct timespec start_time,
 			       struct timespec end_time)
@@ -77,16 +81,69 @@ int getdents64_main(int argc, char *argv[])
 	start_timer(&start_time);
 	// calc time for 100*1000 getdents64
 	for (int i = 0; i < iter; i++) {
-			syscall(SYS_getdents64, fd, buf, BUF_SIZE);
+		syscall(SYS_getdents64, fd, buf, BUF_SIZE);
 	}
 	end_timer(&end_time);
 	double time = get_elapsed_time(start_time, end_time);
 	printf("Average time usage %lf ns, iter %d times\n\n",
 	       (time) / iter * 1000000000.0, iter);
-	exit(EXIT_SUCCESS);
+	close(fd);
+	return 0;
+}
+
+int newfsstatat_main(int argc, char *argv[])
+{
+	struct stat statbuf;
+	int dfd; // Directory file descriptor
+	int sflag = AT_SYMLINK_NOFOLLOW; // Do not follow symbolic links
+
+	// Open the directory to get a file descriptor for it
+	dfd = open(argc > 1 ? argv[1] : ".", O_RDONLY | O_DIRECTORY);
+	if (dfd == -1) {
+		perror("open");
+		exit(EXIT_FAILURE);
+	}
+	const char *file_name = argc > 2 ? argv[2] : ".profile";
+	// Use fstatat to get information about a file within that directory
+	if (fstatat(dfd, file_name, &statbuf, sflag) == -1) {
+		perror("newfstatat");
+		close(dfd);
+		exit(EXIT_FAILURE);
+	}
+
+	// Output some of the file information
+	printf("File size: %lld bytes\n", (long long)statbuf.st_size);
+	printf("File inode: %ld\n", (long)statbuf.st_ino);
+
+	int iter = 100000;
+	struct timespec start_time, end_time;
+	start_timer(&start_time);
+	// calc time for 100*1000 getdents64
+	for (int i = 0; i < iter; i++) {
+		fstatat(dfd, file_name, &statbuf, sflag);
+	}
+	end_timer(&end_time);
+	double time = get_elapsed_time(start_time, end_time);
+	printf("Average time usage %lf ns, iter %d times\n\n",
+	       (time) / iter * 1000000000.0, iter);
+	// Close the directory file descriptor
+	close(dfd);
+
+	return 0;
 }
 
 int main(int argc, char *argv[])
 {
-	return getdents64_main(argc, argv);
+	if (argc <= 1 || strcmp(argv[1], "--help") == 0) {
+		printf("Usage: %s [getdents64|newfsstatat]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	if (strcmp(argv[1], "getdents64") == 0) {
+		return getdents64_main(argc - 1, argv + 1);
+	} else if (strcmp(argv[1], "newfsstatat") == 0) {
+		return newfsstatat_main(argc - 1, argv + 1);
+	} else {
+		printf("Usage: %s [getdents64|newfsstatat]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 }
