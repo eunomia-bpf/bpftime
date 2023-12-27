b01414f398312642f6f93a99756e4fcdefa4b061
officeyutong <yt.xyxx@gmail.com>
2023-12-27 21:13:14 +0800

    Update loading

--- vm/cli/main.cpp
+++ vm/cli/main.cpp
@@ -3,15 +3,22 @@
 #include "spdlog/spdlog.h"
 #include <argparse/argparse.hpp>
 #include <cstdarg>
+#include <cstdint>
 #include <cstdio>
 #include <fcntl.h>
 #include <filesystem>
+#include <iostream>
 #include <libelf.h>
+#include <llvm/ExecutionEngine/Orc/LLJIT.h>
+#include <llvm/Support/MemoryBuffer.h>
 #include <llvm/llvm_jit_context.hpp>
 #include <string>
 #include <unistd.h>
 #include <bpf/libbpf.h>
 #include <fstream>
+using namespace llvm;
+using namespace llvm::orc;
+
 extern "C" int _libbpf_print(libbpf_print_level level, const char *fmt,
 			     va_list ap)
 {
@@ -70,16 +77,58 @@ static int build_ebpf_program(const std::string &ebpf_elf,
 	}
 	return 0;
 }
-
-static int run_ebpf_program(const std::filesystem::path &elf)
+static ExitOnError exit_on_error;
+using bpf_func = uint64_t (*)(const void *, uint64_t);
+static int run_ebpf_program(const std::filesystem::path &elf,
+			    std::optional<std::string> memory)
 {
-	return 0;
+	auto jit = exit_on_error(LLJITBuilder().create());
+
+	if (auto file_buf = MemoryBuffer::getFile(elf.c_str()); file_buf) {
+		exit_on_error(jit->addObjectFile(std::move(*file_buf)));
+	} else {
+		SPDLOG_ERROR("Unable to read elf file: {}",
+			     file_buf.getError().message());
+		return 1;
+	}
+	if (auto ret = jit->lookup("bpf_main"); ret) {
+		auto func = ret->toPtr<bpf_func>();
+		uint64_t result;
+		if (memory.has_value()) {
+			std::ifstream ifs(*memory,
+					  std::ios::binary | std::ios::ate);
+			if (!ifs.is_open()) {
+				SPDLOG_ERROR("Unable to open memory file");
+				return 1;
+			}
+			std::streamsize size = ifs.tellg();
+			ifs.seekg(0, std::ios::beg);
+			std::vector<uint8_t> buffer(size);
+			if (!ifs.read((char *)buffer.data(), size)) {
+				SPDLOG_ERROR("Unable to read memory");
+				return 1;
+			}
+			SPDLOG_INFO("Memory size: {}", size);
+			result = func(buffer.data(), buffer.size());
+		} else {
+			result = func(nullptr, 0);
+		}
+		SPDLOG_INFO("Output: {}", result);
+		return 0;
+	} else {
+		std::string buf;
+		raw_string_ostream os(buf);
+		os << ret.takeError();
+		SPDLOG_ERROR("Unable to lookup bpf_main: {}", buf);
+		return 1;
+	}
 }
 
 int main(int argc, const char **argv)
 {
 	libbpf_set_print(_libbpf_print);
-
+	InitializeNativeTarget();
+	InitializeNativeTargetAsmPrinter();
 	argparse::ArgumentParser program(argv[0]);
 
 	argparse::ArgumentParser build_command("build");
@@ -94,7 +143,9 @@ int main(int argc, const char **argv)
 	argparse::ArgumentParser run_command("run");
 	run_command.add_description("Run an native eBPF program");
 	run_command.add_argument("PATH").help("Path to the ELF file");
-
+	run_command.add_argument("MEMORY")
+		.help("Path to the memory file")
+		.nargs(0, 1);
 	program.add_subparser(build_command);
 	program.add_subparser(run_command);
 	try {
@@ -113,7 +164,14 @@ int main(int argc, const char **argv)
 			build_command.get<std::string>("EBPF_ELF"),
 			build_command.get<std::string>("output"));
 	} else if (program.is_subcommand_used(run_command)) {
-		return run_ebpf_program(run_command.get<std::string>("PATH"));
+		if (run_command.is_used("MEMORY")) {
+			return run_ebpf_program(
+				run_command.get<std::string>("PATH"),
+				run_command.get<std::string>("MEMORY"));
+		} else {
+			return run_ebpf_program(
+				run_command.get<std::string>("PATH"), {});
+		}
 	}
 	return 0;
 }
