92e7acea4ef1745f1b8241ff4e20f02adbe2530c
officeyutong <yt.xyxx@gmail.com>
2024-01-15 00:32:33 +0800

    Add usdt example

---
+++ example/usdt_minimal/victim.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ostream>
+#include <sys/sdt.h>
+#include <random>
+#include <thread>
+using namespace std::chrono_literals;
+int main()
+{
+	std::mt19937 gen;
+	gen.seed(std::random_device()());
+	std::uniform_int_distribution<int> rand(1, 1e6);
+	while (true) {
+		int x = rand(gen);
+		int y = rand(gen);
+		int z = x + y;
+		DTRACE_PROBE3(victim, probe1, x, y, z);
+		std::cout << x << " + " << y << " = " << z << std::endl;
+		std::this_thread::sleep_for(1s);
+	}
+	return 0;
+}
