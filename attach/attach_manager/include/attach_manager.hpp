511607f53911835b61d5fd31abe0137d516d8028
officeyutong <yt.xyxx@gmail.com>
2024-02-18 18:03:16 +0800

    basic update

---
+++ attach/attach_manager/include/attach_manager.hpp
@@ -0,0 +1,8 @@
+#ifndef _BPFTIME_ATTACH_MANAGER_HPP
+#define _BPFTIME_ATTACH_MANAGER_HPP
+namespace bpftime {
+    namespace attach {
+        
+    }
+}
+#endif
---
+++ attach/attach_manager/include/instantiated_program.hpp
@@ -0,0 +1,10 @@
+#ifndef _BPFTIME_INSTANTIATED_PROGRAM_HPP
+#define _BPFTIME_INSTANTIATED_PROGRAM_HPP
+namespace bpftime
+{
+namespace attach
+{
+
+}
+} // namespace bpftime
+#endif
