c081dd829d4e0a5b050213b21debf4e2a9255476
云微 <1067852565@qq.com>
2024-02-28 12:17:54 +0100

    runtime: add map_ops support (#229)
    * remove aot cache for security issues
    
    * remove helper map ops
    
    * rename from_userspace to from_syscall
    
    * add support for NON_BUILDIN_MAP_TYPES
    
    * add support for map ops
    
    * fix tests for bpftime_register_map_ops
    
    * fix include
    
    * fix warning

--- runtime/include/bpftime_shm.hpp
+++ runtime/include/bpftime_shm.hpp
@@ -102,6 +102,7 @@ enum class bpf_map_type {
 	BPF_MAP_TYPE_KERNEL_USER_PERF_EVENT_ARRAY =
 		KERNEL_USER_MAP_OFFSET + BPF_MAP_TYPE_PERF_EVENT_ARRAY,
 
+	BPF_MAP_TYPE_MAX = 2048,
 };
 
 enum class shm_open_type {
@@ -151,6 +152,32 @@ extern const shm_open_type global_shm_open_type;
 const bpftime::agent_config &set_agent_config_from_env();
 const bpftime::agent_config &bpftime_get_agent_config();
 void bpftime_set_agent_config(bpftime::agent_config &cfg);
+
+// Map ops for register external map types and operations
+//
+// The map ops will be store in each process, it's not in global shared memory
+// Once the map operates on non-builtin map type, it will find the map ops
+// in the map ops table and call the corresponding functions.
+struct bpftime_map_ops {
+	// initialize the map
+	int (*alloc_map)(int id, const char *name, bpf_map_attr attr);
+	// free the map
+	void (*map_free)(int id);
+	// lookup the elem
+	void *(*elem_lookup)(int id, const void *key, bool from_syscall);
+	// update the elem
+	long (*elem_update)(int id, const void *key, const void *value, uint64_t flags, bool from_syscall);
+	// delete the elem
+	long (*elem_delete)(int id, const void *key, bool from_syscall);
+	// get the next key
+	int (*map_get_next_key)(int id, const void *key, void *next_key, bool from_syscall);
+};
+
+// register new map ops
+//
+// External map ops needs the syscall-server is running with the map ops enabled.
+int bpftime_register_map_ops(int map_type, bpftime_map_ops *ops);
+
 } // namespace bpftime
 
 extern "C" {
@@ -251,16 +278,6 @@ int bpftime_map_get_info(int fd, bpftime::bpf_map_attr *out_attr,
 
 // get the map value size from the global shared memory by fd
 uint32_t bpftime_map_value_size_from_syscall(int fd);
-
-// used by bpf_helper to get the next key
-int bpftime_helper_map_get_next_key(int fd, const void *key, void *next_key);
-// used by bpf_helper to lookup the elem
-const void *bpftime_helper_map_lookup_elem(int fd, const void *key);
-// used by bpf_helper to update the elem
-long bpftime_helper_map_update_elem(int fd, const void *key, const void *value,
-				    uint64_t flags);
-// used by bpf_helper to delete the elem
-long bpftime_helper_map_delete_elem(int fd, const void *key);
 
 // use from bpf syscall to get the next key
 int bpftime_map_get_next_key(int fd, const void *key, void *next_key);
---
+++ runtime/unit-test/maps/test_external_map_ops.cpp
@@ -0,0 +1,92 @@
+#include "catch2/catch_message.hpp"
+#include "../common_def.hpp"
+#include <boost/interprocess/creation_tags.hpp>
+#include <boost/interprocess/interprocess_fwd.hpp>
+#include <bpf_map/userspace/per_cpu_hash_map.hpp>
+#include "bpftime_config.hpp"
+#include "bpftime_shm.hpp"
+#include "bpftime_handler.hpp"
+#include <catch2/catch_test_macros.hpp>
+#include <cstdint>
+#include <sched.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <vector>
+#include <bpf_map/map_common_def.hpp>
+#include "catch2/internal/catch_run_context.hpp"
+#include <algorithm>
+#include <random>
+#include <map>
+
+using namespace boost::interprocess;
+using namespace bpftime;
+
+static const char *SHM_NAME = "BPFTIME_TEST_ENTERNAL_MAP_SHM";
+static const char *HANDLER_NAME = "my_handler";
+static const int test_map_type = 2000;
+
+static std::map<uint32_t, uint64_t> cpp_map;
+
+static void *elem_lookup_static(int id, const void *key, bool from_syscall)
+{
+	auto k = *(uint32_t *)key;
+	auto it = cpp_map.find(k);
+	if (it == cpp_map.end())
+		return nullptr;
+	return &it->second;
+}
+
+static long elem_update_static(int id, const void *key, const void *value,
+			       uint64_t flags, bool from_syscall)
+{
+	auto k = *(uint32_t *)key;
+	auto v = *(uint32_t *)value;
+	cpp_map[k] = v;
+	return 0;
+}
+
+TEST_CASE("Test basic operations of external hash map ops")
+{
+	struct bpftime::shm_remove remover(SHM_NAME);
+	// The side that creates the mapping
+	managed_shared_memory segment(create_only, SHM_NAME, 1 << 20);
+	auto manager =
+		segment.construct<handler_manager>(HANDLER_NAME)(segment);
+	auto &manager_ref = *manager;
+
+	// update the ops to use the external map
+	bpftime::agent_config agent_config;
+	agent_config.allow_non_buildin_map_types = true;
+	bpftime_set_agent_config(agent_config);
+
+	cpp_map.clear();
+
+	bpftime_map_ops map_ops{
+		.elem_lookup = elem_lookup_static,
+		.elem_update = elem_update_static,
+	};
+
+	int res = manager_ref.set_handler(1,
+					  bpf_map_handler(1, test_map_type, 4,
+							  8, 1024, 0, "hash1",
+							  segment),
+					  segment);
+	REQUIRE(res == 1);
+
+	REQUIRE(bpftime_register_map_ops(test_map_type, &map_ops) == 0);
+	// test insert
+	auto &map_handler = std::get<bpf_map_handler>(manager_ref[1]);
+	for (uint64_t i = 0; i < 100; i++) {
+		uint32_t key = i;
+		uint64_t value = i;
+		map_handler.map_update_elem(&key, &value, 0);
+	}
+
+	// test lookup
+	for (uint64_t i = 0; i < 100; i++) {
+		uint32_t key = i;
+		auto val = *(uint64_t *)(map_handler.map_lookup_elem(&key));
+		REQUIRE(val == i);
+		spdlog::info("val for {} = {:x}", i, val);
+	}
+}
