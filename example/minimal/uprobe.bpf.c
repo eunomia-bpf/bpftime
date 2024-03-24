53a16b554cc4f7083e8209fe1f2b503132b4a4b8
yunwei37 <yunwei356@gmail.com>
2024-03-24 22:31:27 +0000

    fix uprobe example

--- example/minimal/uprobe.bpf.c
+++ example/minimal/uprobe.bpf.c
@@ -1,10 +1,13 @@
+#define BPF_NO_GLOBAL_DATA
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
 
-
-int _bpf_helper_ext_0006(char* arg0);
-
-int bpf_main(void *ctx)
+SEC("uprobe/./victim:target_func")
+int do_uprobe_trace(struct pt_regs *ctx)
 {
-	_bpf_helper_ext_0006("target_func called.\n");
+	bpf_printk("target_func called.\n");
 	return 0;
 }
 
+char LICENSE[] SEC("license") = "GPL";
