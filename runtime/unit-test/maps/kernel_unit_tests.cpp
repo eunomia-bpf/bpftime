f66e70d04e8fc5e0d14639ec0fa4340f1d2a094b
kenlig <28685375+kenlig@users.noreply.github.com>
2024-10-19 23:56:00 +0800

    update

--- runtime/unit-test/maps/kernel_unit_tests.cpp
+++ runtime/unit-test/maps/kernel_unit_tests.cpp
@@ -1,3 +1,4 @@
+#include "bpf_map/userspace/per_cpu_hash_map.hpp"
 #include "catch2/catch_test_macros.hpp"
 #include "linux/bpf.h"
 #include "unit-test/common_def.hpp"
@@ -5,6 +6,8 @@
 #include <boost/interprocess/interprocess_fwd.hpp>
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include "bpf_map/userspace/var_hash_map.hpp"
+#include <cstring>
+#include <unistd.h>
 static const char *SHM_NAME = "_HASH_MAP_TEST";
 
 using namespace boost::interprocess;
@@ -105,4 +108,145 @@ TEST_CASE("Test hash map (kernel)")
 	REQUIRE(errno == ENOENT);
 	REQUIRE(map.map_get_next_key(&key, &next_key) < 0);
 	REQUIRE(errno == ENOENT);
+}
+
+TEST_CASE("test_hashmap_sizes (kernel)")
+{
+	shm_remove remover(SHM_NAME);
+	managed_shared_memory mem(boost::interprocess::create_only, SHM_NAME,
+				  20 << 20);
+	int i, j;
+
+	for (i = 1; i <= 512; i <<= 1)
+		for (j = 1; j <= 1 << 18; j <<= 1) {
+			var_size_hash_map_impl map(mem, i, j, 2);
+			usleep(10);
+		}
+}
+
+/** Macros from kernel source */
+#define __bpf_percpu_val_align __attribute__((__aligned__(8)))
+
+#define BPF_DECLARE_PERCPU(type, name)                                         \
+	struct {                                                               \
+		type v; /* padding */                                          \
+	} __bpf_percpu_val_align name[sysconf(_SC_NPROCESSORS_ONLN)]
+#define bpf_percpu(name, cpu) name[(cpu)].v
+
+TEST_CASE("test_hashmap_percpu (kernel)")
+{
+	shm_remove remover(SHM_NAME);
+	managed_shared_memory mem(boost::interprocess::create_only, SHM_NAME,
+				  20 << 20);
+
+	unsigned int nr_cpus = sysconf(_SC_NPROCESSORS_ONLN);
+	BPF_DECLARE_PERCPU(long, value);
+	long long key, next_key, first_key;
+	int expected_key_mask = 0;
+	int i;
+	per_cpu_hash_map_impl map(mem, sizeof(key),
+				  sizeof(bpf_percpu(value, 0)), 2);
+
+	for (i = 0; i < (int)nr_cpus; i++)
+		bpf_percpu(value, i) = i + 100;
+
+	key = 1;
+	/* Insert key=1 element. */
+	REQUIRE(!(expected_key_mask & key));
+	REQUIRE(map.elem_update_userspace(&key, value, BPF_ANY) == 0);
+
+	/* Lookup and delete elem key=1 and check value. */
+	REQUIRE((map.lookup_and_delete_userspace(&key, value) == 0 &&
+		 bpf_percpu(value, 0) == 100));
+
+	for (i = 0; i < (int)nr_cpus; i++)
+		bpf_percpu(value, i) = i + 100;
+
+	/* Insert key=1 element which should not exist. */
+	REQUIRE(map.elem_update_userspace(&key, value, BPF_NOEXIST) == 0);
+	expected_key_mask |= key;
+
+	/* BPF_NOEXIST means add new element if it doesn't exist. */
+	REQUIRE((map.elem_update_userspace(&key, value, BPF_NOEXIST) < 0 &&
+		 /* key=1 already exists. */
+		 errno == EEXIST));
+
+	/* -1 is an invalid flag. */
+	REQUIRE((map.elem_update(&key, value, -1) < 0 && errno == EINVAL));
+
+	const auto lookup_helper = [&](const void *key, void *value) -> long {
+		auto returned_value = map.elem_lookup_userspace(key);
+		if (!returned_value)
+			return -1;
+		memcpy(value, returned_value,
+		       map.get_value_size() * map.getncpu());
+		return 0;
+	};
+
+	/* Check that key=1 can be found. Value could be 0 if the lookup
+	 * was run from a different CPU.
+	 */
+	bpf_percpu(value, 0) = 1;
+	REQUIRE((lookup_helper(&key, value) == 0 &&
+		 bpf_percpu(value, 0) == 100));
+
+	key = 2;
+	/* Check that key=2 is not found. */
+	REQUIRE((lookup_helper(&key, value) < 0 && errno == ENOENT));
+
+	/* BPF_EXIST means update existing element. */
+	REQUIRE((map.elem_update_userspace(&key, value, BPF_EXIST) < 0 &&
+		 /* key=2 is not there. */
+		 errno == ENOENT));
+
+	/* Insert key=2 element. */
+	REQUIRE(!(expected_key_mask & key));
+	REQUIRE(map.elem_update_userspace(&key, value, BPF_NOEXIST) == 0);
+	expected_key_mask |= key;
+
+	/* key=1 and key=2 were inserted, check that key=0 cannot be
+	 * inserted due to max_entries limit.
+	 */
+	key = 0;
+	REQUIRE((map.elem_update_userspace(&key, value, BPF_NOEXIST) < 0 &&
+		 errno == E2BIG));
+
+	/* Check that key = 0 doesn't exist. */
+	REQUIRE((map.elem_delete_userspace(&key) < 0 && errno == ENOENT));
+
+	/* Iterate over two elements. */
+	REQUIRE((map.map_get_next_key(NULL, &first_key) == 0 &&
+		 ((expected_key_mask & first_key) == first_key)));
+	while (!map.map_get_next_key(&key, &next_key)) {
+		if (first_key) {
+			REQUIRE(next_key == first_key);
+			first_key = 0;
+		}
+		REQUIRE((expected_key_mask & next_key) == next_key);
+		expected_key_mask &= ~next_key;
+
+		REQUIRE(lookup_helper(&next_key, value) == 0);
+
+		for (i = 0; i < (int)nr_cpus; i++)
+			REQUIRE(bpf_percpu(value, i) == i + 100);
+
+		key = next_key;
+	}
+	REQUIRE(errno == ENOENT);
+
+	/* Update with BPF_EXIST. */
+	key = 1;
+	REQUIRE(map.elem_update_userspace(&key, value, BPF_EXIST) == 0);
+
+	/* Delete both elements. */
+	key = 1;
+	REQUIRE(map.elem_delete_userspace(&key) == 0);
+	key = 2;
+	REQUIRE(map.elem_delete_userspace(&key) == 0);
+	REQUIRE((map.elem_delete_userspace(&key) < 0 && errno == ENOENT));
+
+	key = 0;
+	/* Check that map is empty. */
+	REQUIRE((map.map_get_next_key(NULL, &next_key) < 0 && errno == ENOENT));
+	REQUIRE((map.map_get_next_key(&key, &next_key) < 0 && errno == ENOENT));
 }
