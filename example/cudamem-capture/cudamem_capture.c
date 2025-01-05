9a0f7dde8ac6ea08c1c3c63ad00c333479b039fe
victoryang00 <yangyiwei2000@gmail.com>
2025-01-04 22:25:12 -0800

    [test] add new test for cuda

---
+++ example/cudamem-capture/cudamem_capture.c
@@ -0,0 +1,73 @@
+/* SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2022, eunomia-bpf org
+ * All rights reserved.
+ */
+#include <signal.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdint.h>
+#include <sys/resource.h>
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "error_inject.skel.h"
+#include <inttypes.h>
+#include "attach_override.h"
+
+#define warn(...) fprintf(stderr, __VA_ARGS__)
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format,
+			   va_list args)
+{
+	return vfprintf(stderr, format, args);
+}
+
+static volatile bool exiting = false;
+
+static void sig_handler(int sig)
+{
+	exiting = true;
+}
+
+int main(int argc, char **argv)
+{
+	struct cudamem_capture_bpf *skel;
+	int err;
+
+	/* Set up libbpf errors and debug info callback */
+	libbpf_set_print(libbpf_print_fn);
+
+	/* Cleaner handling of Ctrl-C */
+	signal(SIGINT, sig_handler);
+	signal(SIGTERM, sig_handler);
+
+	/* Load and verify BPF application */
+	skel = cudamem_capture_bpf__open();
+	if (!skel) {
+		fprintf(stderr, "Failed to open and load BPF skeleton\n");
+		return 1;
+	}
+
+	/* Load & verify BPF programs */
+	err = cudamem_capture_bpf__load(skel);
+	if (err) {
+		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
+		goto cleanup;
+	}
+	err = bpf_prog_attach_uprobe_with_override(
+		bpf_program__fd(skel->progs.do_cudamem_capture_patch), "./victim",
+		"target_func");
+	if (err) {
+		fprintf(stderr, "Failed to attach BPF program\n");
+		goto cleanup;
+	}
+	while (!exiting) {
+		sleep(1);
+	}
+cleanup:
+	/* Clean up */
+	cudamem_capture_bpf__destroy(skel);
+	return err < 0 ? -err : 0;
+}
