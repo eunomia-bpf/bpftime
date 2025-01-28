0b9e46e10209eae98593303fba4cbd257525d4e4
officeyutong <yt.xyxx@gmail.com>
2025-01-28 16:35:13 +0800

    compile ok

--- cmake/cuda.cmake
+++ cmake/cuda.cmake
@@ -2,5 +2,5 @@
 function(find_cuda)
     set(CUDA_LIBRARY_PATH /usr/local/cuda-12.6/targets/x86_64-linux/lib/ /usr/local/cuda-12.6/targets/x86_64-linux/lib/stubs/ PARENT_SCOPE)
     set(CUDA_INCLUDE_PATH /usr/local/cuda-12.6/targets/x86_64-linux/include PARENT_SCOPE)
-    set(CUDA_LIBS cuda cudart PARENT_SCOPE)
+    set(CUDA_LIBS cuda cudart libnvptxcompiler_static.a PARENT_SCOPE)
 endfunction()
