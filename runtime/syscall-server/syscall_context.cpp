d7ab555632b711ea7815d86d153e87c541a28f90
sy03 <1370724210@qq.com>
2025-06-21 19:50:24 +0800

    remove special handler

--- runtime/syscall-server/syscall_context.cpp
+++ runtime/syscall-server/syscall_context.cpp
@@ -376,28 +376,6 @@ long syscall_context::handle_sysbpf(int cmd, union bpf_attr *attr, size_t size)
 					       (long)size);
 		}
 
-		// Special handling for bloom filter
-		// For bloom filters, BPF_MAP_LOOKUP_ELEM maps to peek operation
-		// where the value to check is passed via attr->key (not
-		// attr->value)
-		if (bpftime_is_map_fd(attr->map_fd)) {
-			bpftime::bpf_map_attr map_attr;
-			const char *map_name;
-			bpftime::bpf_map_type map_type;
-			int res = bpftime_map_get_info(attr->map_fd, &map_attr,
-						       &map_name, &map_type);
-			if (res == 0 &&
-			    map_type == bpftime::bpf_map_type::
-						BPF_MAP_TYPE_BLOOM_FILTER) {
-				// For bloom filter: key contains the value to
-				// check
-				long ret = bpftime_map_peek_elem(
-					attr->map_fd,
-					(void *)(uintptr_t)attr->key);
-				return ret;
-			}
-		}
-
 		// Note that bpftime_map_lookup_elem is adapted as a bpf helper,
 		// meaning that it will *return* the address of the matched
 		// value. But here the syscall has a different interface. Here
