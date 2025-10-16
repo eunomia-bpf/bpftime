4ed6e52a160a664870390037b761955339111ee7
Sy03 <1370724210@qq.com>
2025-10-16 12:23:02 +0800

    [~] Rollback former impl releated change

--- runtime/src/attach/bpf_attach_ctx.cpp
+++ runtime/src/attach/bpf_attach_ctx.cpp
@@ -281,10 +281,8 @@ int bpf_attach_ctx::instantiate_bpf_link_handler_at(
 				dynamic_cast<attach::nv_attach_private_data &>(
 					*priv_data);
 			nv_attach_private_data.program_name = prog->prog_name();
-			// Pass device-visible pointer to GPU side (pinned host
-			// memory mapped into device address space)
 			nv_attach_private_data.comm_shared_mem =
-				this->cuda_ctx->cuda_shared_mem_device_pointer;
+				(uintptr_t)this->cuda_ctx->cuda_shared_mem.get();
 			nv_attach_private_data.instructions = prog->get_insns();
 			SPDLOG_INFO(
 				"Loaded {} instructions (original) for cuda ebpf program",
