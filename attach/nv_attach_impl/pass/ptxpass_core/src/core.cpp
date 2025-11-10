11da28d4be264f5bb45f0cd33c5b1cfeedbbd192
Peihao Yang <378974295@qq.com>
2025-11-10 15:00:52 +0800

    feat: support early exit (#459) (#481)
    - Add two bpf helper functions for CUDA.
    - Add an early-exit demo in CUDA examples.
    
    Close #459
    
    Signed-off-by: Forsworns <yangpeihao@sjtu.edu.cn>

--- attach/nv_attach_impl/pass/ptxpass_core/src/core.cpp
+++ attach/nv_attach_impl/pass/ptxpass_core/src/core.cpp
@@ -200,6 +200,8 @@ std::string compile_ebpf_to_ptx_from_words(
 	vm.register_external_function(503, "get_block_idx", (void *)test_func);
 	vm.register_external_function(504, "get_block_dim", (void *)test_func);
 	vm.register_external_function(505, "get_thread_idx", (void *)test_func);
+	vm.register_external_function(507, "cuda_exit", (void *)test_func);
+	vm.register_external_function(508, "get_grid_dim", (void *)test_func);
 
 	vm.load_code(insts, insts_count * sizeof(ebpf_inst));
 	bpftime::llvm_bpf_jit_context ctx(vm);
