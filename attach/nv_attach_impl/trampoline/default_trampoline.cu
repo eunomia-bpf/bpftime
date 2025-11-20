e30999049ffffbfade40386cb65d607c4e61a2a4
Sy03 <1370724210@qq.com>
2025-11-21 01:50:41 +0800

    [~] Fix: Updated buffer size to 1<<25 &Increased default shared memory size to 100MB

--- attach/nv_attach_impl/trampoline/default_trampoline.cu
+++ attach/nv_attach_impl/trampoline/default_trampoline.cu
@@ -18,7 +18,7 @@
 /* clang++-17 -S ./default_trampoline.cu -Wall --cuda-gpu-arch=sm_60 -O2
  * -L/usr/local/cuda/lib64/ -lcudart*/
 // The old 1<<30 value makes the shared segment too large for Boost IPC.
-static constexpr int GPU_HELPER_MAX_BUF = 1 << 26;
+static constexpr int GPU_HELPER_MAX_BUF = 1 << 25;
 
 enum class HelperOperation {
 	MAP_LOOKUP = 1,
